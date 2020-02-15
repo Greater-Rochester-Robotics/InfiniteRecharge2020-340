dbd7c812f2d9b33662132f55649e454172d9fa17
griffin <griffinbrewer339@gmail.com>
2020-02-15 14:37:25 -0500

    gyro works now, testing pathing next

--- 340InfiniteRecharge/src/main/java/frc/robot/commands/ColorWheel/SpinToColor.java
+++ 340InfiniteRecharge/src/main/java/frc/robot/commands/ColorWheel/SpinToColor.java
@@ -32,7 +32,7 @@ private ColorName wantedColor;
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    SmartDashboard.putString("Spinning", "Yes Speen");
+    SmartDashboard.putString("Spinning", "Speen");
     
     colorDetected = RobotContainer.colorWheel.getColor();
     if (DriverStation.getInstance().getGameSpecificMessage().length() > 0) {
@@ -52,5 +52,6 @@ private ColorName wantedColor;
     SmartDashboard.putString("Spinning", "No Speen");
     
     return colorDetected.getTargetColor() == wantedColor;
+
   }
 }
