27ad868e092e1cf45f79db0f2de5d3913aa3d466
Wedgehead99 <bluefish1217@outlook.com>
2020-02-17 22:13:02 -0500

    Changed limeLight height and tested cross field shot *it worked!*

--- 340InfiniteRecharge/src/main/java/frc/robot/subsystems/Limelight.java
+++ 340InfiniteRecharge/src/main/java/frc/robot/subsystems/Limelight.java
@@ -60,7 +60,7 @@ public class Limelight extends SubsystemBase {
   public static int calcHoodRPM(){
     double cameraHeight =  25.5;    //not final value
     double targetHeight = 98; //final value = 98
-    double cameraAngle = 32.1;    //changeable
+    double cameraAngle = 17;    //changeable 32.1
     double distance = ((targetHeight - cameraHeight) / Math.tan(Math.toRadians(cameraAngle + RobotContainer.limelight.verticalAngleToTarget())))-12.5;
     //distance equation: rpm = -0.0000401877572016x^3 +.220114087301592x^2 -33.7714947089957x + 4919.04761904768
     double rpm = (-0.0000401877572016*Math.pow(distance, 3)) + (.220114087301592*Math.pow(distance,2)) - (33.7714947089957*distance)+4919.04761904768;
@@ -74,7 +74,7 @@ public class Limelight extends SubsystemBase {
     //all distance values are in inches
     double cameraHeight =  25.5;    //not final value
     double targetHeight = 98; //final value = 98
-    double cameraAngle = 32.1;    //changeable
+    double cameraAngle = 17;    //changeable
     double distance = ((targetHeight - cameraHeight) / Math.tan(Math.toRadians(cameraAngle + RobotContainer.limelight.verticalAngleToTarget())))-12.5; // Returns distance to target, 12.5 is distance camera is from front? of robot
     // System.out.println("Math = " + Math.tan( Math.toRadians(cameraAngle + RobotContainer.limelight.verticalAngleToTarget()) ) +"   distance = " + Distance);
     SmartDashboard.putString("Distance", ""+distance);
