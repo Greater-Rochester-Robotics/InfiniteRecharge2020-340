d38466a853367b65ce3d017d9d792f162602d573
John <john@team340.com>
2020-02-23 00:23:53 -0500

    Altered two constants for intaking- harvester speed

--- 340InfiniteRecharge/src/main/java/frc/robot/commands/Harvester/IntakeBalls.java
+++ 340InfiniteRecharge/src/main/java/frc/robot/commands/Harvester/IntakeBalls.java
@@ -27,7 +27,7 @@ public class IntakeBalls extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    RobotContainer.harvester.setAxleWheels(11.0);
+    RobotContainer.harvester.setAxleWheels(6.0);
   }
 
   // Called once the command ends or is interrupted.
